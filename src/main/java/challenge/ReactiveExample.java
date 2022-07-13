21662bbb7c7a369d0dd8209bc94f65f0680666fc
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-13 12:47:32 -0500

    Solución casos reactivos y sus respectivas pruebas

--- src/main/java/challenge/ReactiveExample.java
+++ src/main/java/challenge/ReactiveExample.java
@@ -3,31 +3,36 @@ package challenge;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class ReactiveExample {
 
     public static final int VALOR_PERMITIDO = 15;
-    private  Flux<Estudiante> estudianteList;
+    private Flux<Estudiante> estudianteList;
 
     public ReactiveExample() {
         //TODO: convertir los estudiantes a un Flux
 
-       /* estudianteList = List.of(
-                new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
-                new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
-                new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
-                new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
-                new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
+        estudianteList = Flux.fromIterable(
+                List.of(
+                        new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
+                        new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
+                        new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
+                        new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
+                        new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
+                )
         );
-*/
     }
 
     //TODO: suma de puntajes
     public Mono<Integer> sumaDePuntajes() {
-        return null;
+        return estudianteList.map(mapeoDeEstudianteAPuntaje())
+                .reduce(0, Integer::sum);
     }
 
     private Function<Estudiante, Integer> mapeoDeEstudianteAPuntaje() {
@@ -35,37 +40,52 @@ public class ReactiveExample {
     }
 
     //TODO: mayor puntaje de estudiante
-    public Mono<Estudiante> mayorPuntajeDeEstudiante(int limit) {
-        return null;
-
+    public Flux<Estudiante> mayorPuntajeDeEstudiante(int limit) {
+        return estudianteList.sort(Comparator.reverseOrder())
+                .take(limit);
     }
 
-    //TODO: total de asisntencias de estudiantes con mayor puntaje basado en un  valor
-    public Mono<Integer> totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(int valor) {
-        return null;
+    //TODO: total de asistencias de estudiantes con mayor puntaje basado en un  valor
+    public Mono<Integer> totalDeAsistenciasDeEstudiantesConMayorPuntajeDe(int valor) {
+        return estudianteList.filter(estudiante -> estudiante.getPuntaje() > valor)
+                .flatMapIterable(Estudiante::getAsistencias)
+                .reduce(Integer::sum);
     }
 
     //TODO: el estudiante tiene asistencias correctas
     public Mono<Boolean> elEstudianteTieneAsistenciasCorrectas(Estudiante estudiante) {
-        return null;
+        return Mono.just(estudiante)
+                .filter(asistenciasPemitidas())
+                .hasElement();
+    }
+
+    private Predicate<Estudiante> asistenciasPemitidas() {
+        return estudiante -> estudiante.getAsistencias()
+                .stream()
+                .reduce(0, Integer::sum) >= VALOR_PERMITIDO;
     }
 
     //TODO: promedio de puntajes por estudiantes
     public Mono<Double> promedioDePuntajesPorEstudiantes() {
-        return null;
+        return estudianteList.collect(
+                Collectors.averagingInt(Estudiante::getPuntaje)
+        );
     }
 
 
     //TODO: los nombres de estudiante con puntaje mayor a un valor
     public Flux<String> losNombresDeEstudianteConPuntajeMayorA(int valor) {
-        return null;
+        return estudianteList
+                .filter(estudiante -> estudiante.getPuntaje() > valor)
+                .map(Estudiante::getNombre);
     }
 
-
-
     //TODO: estudiantes aprovados
-    public Flux<String> estudiantesAprovados(){
-        return null;
+    public Flux<String> estudiantesAprobados() {
+        return estudianteList.filter(estudiante -> estudiante.getPuntaje() > 60)
+                .doOnNext(estudiante -> estudiante.setAprobado(Boolean.TRUE))
+                .filter(Estudiante::isAprobado)
+                .map(Estudiante::getNombre);
     }
 
 
--- src/test/java/challenge/ReactiveExampleTest.java
+++ src/test/java/challenge/ReactiveExampleTest.java
@@ -1,9 +1,16 @@
 package challenge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -14,32 +21,89 @@ class ReactiveExampleTest {
 
 
     @Test
-    void sumaDePuntajes(){
+    void sumaDePuntajes() {
+        //arrange
+        StepVerifier
+                .create(reactiveExample.sumaDePuntajes()) //act
+                .expectNext(260)//asert
+                .verifyComplete();
+
+        Optional.ofNullable(reactiveExample.sumaDePuntajes().block())
+                .filter(Objects::nonNull)
+                .ifPresentOrElse(puntaje -> {
+                            Assertions.assertEquals(260, puntaje);
+                        },
+                        Assertions::fail
+                );
+    }
+
+    @Test
+    void mayorPuntajeDeEstudiante() {
+        //arrange
+        var estudiante = reactiveExample.mayorPuntajeDeEstudiante(2); //act
+
+        StepVerifier.create(estudiante)
+                .expectNextMatches(validarEstudiante("pedro", 80, 5))//assert
+                .expectNextMatches(validarEstudiante("juan", 75, 5))
+                .expectComplete()
+                .verify();
+    }
+
+    private Predicate<Estudiante> validarEstudiante(String nombre, Integer puntaje, Integer cantidadDeAsistencias) {
+        return estudiante -> estudiante.getNombre().equals(nombre)
+                && estudiante.getPuntaje().equals(puntaje)
+                && estudiante.getAsistencias().size() == cantidadDeAsistencias;
+    }
+
+    @Test
+    void totalDeAsistenciasDeEstudiantesComMayorPuntajeDe() {
+        //arrange
+        var totalAsistencias = reactiveExample.totalDeAsistenciasDeEstudiantesConMayorPuntajeDe(74);//act
+
+        StepVerifier.create(totalAsistencias)
+                .expectNext(43)//assert
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void elEstudianteTieneAsistenciasCorrectas() {
+        //arrange
+        var estudiante = new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5));
+
+        StepVerifier.create(reactiveExample.elEstudianteTieneAsistenciasCorrectas(estudiante))//act
+                .expectNext(Boolean.TRUE)// assert
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void promedioDePuntajesPorEstudiantes() {
+        //arrange
+        var puntaje = 52.0;
+
+        StepVerifier.create(reactiveExample.promedioDePuntajesPorEstudiantes())//act
+                .expectNext(puntaje)//assert
+                .verifyComplete();
+    }
+
+    @Test
+    void estudiantesAprobados() {
+        //arrange
+        StepVerifier.create(reactiveExample.estudiantesAprobados())//act
+                .expectNext("juan")
+                .expectNext("pedro")//assert
+                .expectComplete()
+                .verify();
 
     }
 
     @Test
-    void mayorPuntajeDeEstudiante(){
-
-    }
-
-    @Test
-    void totalDeAsisntenciasDeEstudiantesComMayorPuntajeDe(){
-
-    }
-
-    @Test
-    void elEstudianteTieneAsistenciasCorrectas(){
-
-    }
-
-    @Test
-    void promedioDePuntajesPorEstudiantes(){
-
-    }
-
-    @Test
-    void estudiantesAprovados(){
-
+    void losNombresDeEstudianteConPuntajeMayorA() {
+        StepVerifier
+                .create(reactiveExample.losNombresDeEstudianteConPuntajeMayorA(74))
+                .expectNext("juan", "pedro")
+                .expectComplete()
+                .verify();
     }
 }
